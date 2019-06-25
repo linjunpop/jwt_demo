fe053c1e712f5961b79b59854c873bd74a0fdff1
Jun Lin <linjunpop@gmail.com>
2019-06-25 21:57:24 +0800

    Added sign.

--- lib/jwt_demo.ex
+++ lib/jwt_demo.ex
@@ -6,17 +6,38 @@ defmodule JwtDemo do
   @doc """
   Generate a JWT token from `header` and `payload`
   """
-  @spec generate_jwt(binary(), binary()) :: binary()
-  def generate_jwt(header, payload) do
+  def generate_jwt(payload, sign \\ nil)
+
+  def generate_jwt(payload, nil) when is_map(payload) do
     [
-      encode(header),
-      encode(payload)
+      encode_map(%{alg: "none"}),
+      encode_map(payload)
     ]
     |> Enum.join(".")
   end
 
-  defp encode(data) do
+  def generate_jwt(payload, {:sha256, secret_key}) do
+    header_and_payload =
+      [
+        encode_map(%{alg: "HS256"}),
+        encode_map(payload)
+      ]
+      |> Enum.join(".")
+
+    signature = :crypto.hmac(:sha256, secret_key, header_and_payload)
+
+    [header_and_payload, base64_encode(signature)]
+    |> Enum.join(".")
+  end
+
+  defp encode_map(data) when is_map(data) do
     data
+    |> Jason.encode!()
+    |> base64_encode()
+  end
+
+  defp base64_encode(string) do
+    string
     |> Base.url_encode64()
     # remove trailing equal sign
     |> String.replace(~r/=$/, "")
--- test/jwt_demo_test.exs
+++ test/jwt_demo_test.exs
@@ -4,21 +4,24 @@ defmodule JwtDemoTest do
 
   describe "generate_jwt/2" do
     test "generate unsecured JWT" do
-      header = ~S"""
-      {
-        "alg": "none"
+      payload = %{
+        sub: "user-123123123"
       }
-      """
 
-      payload = ~S"""
-      {
-        "sub": "user-123123123"
+      result = JwtDemo.generate_jwt(payload)
+
+      assert result == "eyJhbGciOiJub25lIn0.eyJzdWIiOiJ1c2VyLTEyMzEyMzEyMyJ9"
+    end
+
+    test "generate signed JWT" do
+      payload = %{
+        sub: "user-123123123"
       }
-      """
 
-      result = JwtDemo.generate_jwt(header, payload)
+      result = JwtDemo.generate_jwt(payload, {:sha256, "my-secret"})
 
-      assert result == "ewogICJhbGciOiAibm9uZSIKfQo.ewogICJzdWIiOiAidXNlci0xMjMxMjMxMjMiCn0K"
+      assert result ==
+               "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLTEyMzEyMzEyMyJ9.JXmdNRfwZmq3qAYZAVjXk0crv9axjR7HfwnNoF87qnc"
     end
   end
 end
